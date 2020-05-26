d0b3fca8d57e3f923419c0ef78e631212d2a3ed0
薄浩宇 <123456>
2020-05-26 10:44:34 +0800

    1

--- xcx_workman1/utils/configzpy.js
+++ xcx_workman1/utils/configzpy.js
@@ -1,6 +1,6 @@
 var api = require('./config.js')
 //const baseUrl = "http://192.168.1.242:8081/work-boot/"
-const baseUrl = "http://192.168.1.237:8081/work-boot/"
+const baseUrl = "http://192.168.1.230:3000/work-boot/"
 // 需求列表
 // const xqurl = baseUrl + "work/yneed/list"
 // 大厅最新需求
@@ -9,9 +9,18 @@ const zuixinxq = baseUrl + "pc/hall/yneedList"
 const tjsp = baseUrl + "pc/hall/userGoodPage"
 // 工人晒晒
 const CasePage = baseUrl + "pc/hall/casePage"
+// 需求详情
+const needSignPage = baseUrl + "pc/hall/needSignPage"
+// 需求删除
+const delYneedAndNeedSign = baseUrl + "pc/hall/delYneedAndNeedSign"
+// 需求完成
+const needUpdateStateById = baseUrl + "pc/hall/needUpdateStateById"
 module.exports = {
   CasePage:CasePage,
   //xqurl:xqurl,
-  zuixinxq:zuixinxq,
+  zuixinxq:zuixinxq,
+  needSignPage:needSignPage,
+  delYneedAndNeedSign:delYneedAndNeedSign,
+  needUpdateStateById:needUpdateStateById,
   tjsp:tjsp
 };
