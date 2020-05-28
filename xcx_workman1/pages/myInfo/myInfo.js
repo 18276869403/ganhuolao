0ada81aab13dc1a3d127f513ac8fc9f2e05da637
邓星玉 <1336797684@qq.com>
2020-05-28 15:24:12 +0800

    1

--- xcx_workman1/pages/myInfo/myInfo.js
+++ xcx_workman1/pages/myInfo/myInfo.js
@@ -1,6 +1,6 @@
 // pages/myInfo/myInfo.js
 var app = getApp()
-
+var qingqiu = require('../../utils/request.js')
 Page({
 
   /**
@@ -24,6 +24,28 @@ Page({
     this.setData({
       wxUser:obj
     })
+    console.log(this.data.wxUser)
+  },
+
+  // 保存
+  UpdateUserInfo:function(){
+    var that = this
+    var data = {
+      id:that.data.wxUser.id,
+      name:that.data.wxUser.name,
+      dateBirth:that.data.wxUser.dateBirth,
+      sex:that.data.wxUser.sex,
+      phone:that.data.wxUser.phone
+    }
+    qingqiu.get("editWxUser",data,function(re){
+      if (re.success == true) {
+        if (re.result != null) {
+          wx.navigateTo({
+            url: '../mine/mine'
+          })
+        }
+      } 
+    },'put')
   },
 
   //性别选择
@@ -33,8 +55,9 @@ Page({
       itemList: that.data.itemList,
       success(res) {
         console.log(res.tapIndex)
+        var sex = "wxUser.sex"
         that.setData({
-          sex: res.tapIndex
+          [sex]: res.tapIndex
         })
       },
       fail(res) {
@@ -45,20 +68,23 @@ Page({
   //日期选择器
   bindDateChange(e) {
     console.log('picker发送选择改变，携带值为', e.detail.value)
+    var dateBirth = "wxUser.dateBirth"
     this.setData({
-      date: e.detail.value
+      [dateBirth] : e.detail.value
     })
   },
   //获取输入的名称
   nameinput: function (e) {
+    var name = "wxUser.name"
     this.setData({
-      name: e.detail.value
+      [name]: e.detail.value
     })
   },
   //获取输入的手机号
   phoneinput: function (e) {
+    var phone = "wxUser.phone"
     this.setData({
-      phone: e.detail.value
+      [phone]: e.detail.value
     })
   },
 })
