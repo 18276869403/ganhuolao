bcc4084415aadc9acefdcea60fb9964d1950ba2a
邓星玉 <1336797684@qq.com>
2020-05-28 08:57:50 +0800

    1

--- xcx_workman1/utils/configzpy.js
+++ xcx_workman1/utils/configzpy.js
@@ -4,7 +4,7 @@ const baseUrl = "http://192.168.1.254:3000/work-boot/"
 // 需求列表
 // const xqurl = baseUrl + "work/yneed/list"
 // 大厅最新需求
-const zuixinxq = baseUrl + "pc/hall/yneedList"
+const zuixinxq = baseUrl + "pc/hall/yneedList" 
 // 推荐商品
 const tjsp = baseUrl + "pc/hall/userGoodPage"
 // 工人晒晒
