a27e0a4d2354901340d3d89179777bf2b41c7733
薄浩宇 <123456>
2020-05-29 17:58:41 +0800

    1

---
+++ xcx_workman1/pages/showDetails/showDetails.js
@@ -0,0 +1,90 @@
+// pages/showDetails/showDetails.js
+const app = getApp()
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    imgUrl: '',
+    bannerImg: [{
+        id: 1,
+        bannerimg: '../image/top.png'
+      },
+      {
+        id: 2,
+        bannerimg: '../image/top.png'
+      },
+      {
+        id: 3,
+        bannerimg: '../image/top.png'
+      }
+    ],
+    pinglunList: [{
+        id: 1,
+        img: '../image/tu.png',
+        nickname: '干活佬220用户',
+        time: '2019.11.15 12:00',
+        details: '很好，很漂亮'
+      },
+      {
+        id: 1,
+        img: '../image/tu.png',
+        nickname: '干活佬220用户',
+        time: '2019.11.15 12:00',
+        details: '很好，很漂亮'
+      },
+      {
+        id: 1,
+        img: '../image/tu.png',
+        nickname: '干活佬220用户',
+        time: '2019.11.15 12:00',
+        details: '很好，很漂亮'
+      }
+    ],
+    id: 0,
+    wxCaseVo: null,
+    caseMsgList: [{
+      id: 1,
+      picUrl: '../image/top.png',
+      wxNc: 'ssss',
+      createTime: '2019.11.15 12:00',
+      content: '室内F-10木门（含五金件）室内F-10木门（含五金件）室内F-10木门（含五金件）'
+    }]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function(options) {
+    this.data.id = options.id
+    this.getdata()
+    this.setData({
+      imgUrl: api.imgUrl
+    })
+  },
+
+  onShow: function() {
+
+  },
+  fangda: function(e) {
+    var img = e.currentTarget.dataset.img
+    wx.previewImage({
+      current: img, // 当前显示图片的http链接
+      urls: this.data.bannerImg // 需要预览的图片http链接列表
+    })
+  },
+
+  //跳转到添加评论页面
+  comment: function() {
+    wx.redirectTo({
+      url: '../comment/comment?id=' + this.data.id,
+    })
+  },
+  lianxita: function(e) {
+    wx.makePhoneCall({
+      phoneNumber: '19191919919'
+    })
+  },
+
+})
