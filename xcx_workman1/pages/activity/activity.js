ba77ded6f5ffad4730723b8ca6ec7c80b59cacc6
邓星玉 <1336797684@qq.com>
2020-06-01 19:49:08 +0800

    1

--- xcx_workman1/pages/activity/activity.js
+++ xcx_workman1/pages/activity/activity.js
@@ -1,48 +1,73 @@
 // pages/activity/activity.js
+const qingqiu = require('../../utils/request.js')
+const api = require('../../utils/config.js')
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    height:''
+    codew:'',
+    codeh:'',
+    viewUrl:api.viewUrl,
+    imgurl:'',
+    imageurl:''
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    var that = this
+    console.log(getApp().globalData.openid)
     this.setData({
-      height: wx.getSystemInfoSync().windowHeight,　　//屏幕高度
-      width: wx.getSystemInfoSync().windowWidth　　//屏幕宽度
+      codeh: wx.getSystemInfoSync().windowHeight,　　//屏幕高度
+      codew: wx.getSystemInfoSync().windowWidth　　//屏幕宽度
     })
-    console.log(this.data.height)
+    var data = {
+      openid:getApp().globalData.openid
+    }
+    qingqiu.get("getCode",data,function(res){
+      that.setData({
+        imgurl:res.twoCodeUrl
+      })
+      that.getImg()
+    })
   },
 
-  getCode: function(){
-    wx.request({
-      url: 'https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=wx14e076d27e942480&secret=fb16e928e1a41fa0e8f21b2f50aa89d5',
-      method: 'get',
-      success: function (res) {
-        let data = {
-          scene: '123456', // 要传递的参数
-          page: 'pages/index/index', // 扫码进入的小程序页面
+  getImg:function(){
+    var that = this
+    const ctx = wx.createCanvasContext('shareCanvas')
+    var bgImgPath = that.data.viewUrl + '7621590993558_.pic_hd.jpg'
+    wx.downloadFile({
+      url: bgImgPath, //仅为示例，并非真实的资源
+      success (res) {
+        // 只要服务器有响应数据，就会把响应内容写入文件并进入 success 回调，业务需要自行判断是否下载到了想要的内容
+        if (res.statusCode === 200) {
+          wx.getImageInfo({
+            src: res.tempFilePath,
+            success:function(res){
+              let imgPath = res.path
+              ctx.drawImage(imgPath, 0, 0, that.data.codew, that.data.codeh)
+              wx.downloadFile({
+                url: that.data.viewUrl + that.data.imgurl, //仅为示例，并非真实的资源
+                success (res) {
+                  // 只要服务器有响应数据，就会把响应内容写入文件并进入 success 回调，业务需要自行判断是否下载到了想要的内容
+                  if (res.statusCode === 200) {
+                    wx.getImageInfo({
+                      src: res.tempFilePath,
+                      success:function(res){
+                          // 绘制二维码
+                          ctx.drawImage(res.path, 50, that.data.codeh - 320, 230, 230)
+                          ctx.draw()
+                      }
+                    })
+                  }
+                }
+              })
+            }
+          })
         }
-    wx.request({
-          url: 'https://api.weixin.qq.com/wxa/getwxacodeunlimit?access_token='+res.data.access_token,
-          method: 'post',
-          data:data,
-          // dataType: 'json',
-          responseType: 'arraybuffer',         //将返回数据 按文本解析修改为arraybuffer
-          success: function (res) {
-            console.log()
-            self.setData({
-               //再arraybuffer转为base64  显示图片
-              url: 'data:image/png;base64,'+wx.arrayBufferToBase64(res.data)
-            })
-          }
-        })
       }
     })
   }
