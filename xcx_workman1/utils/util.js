58a7aa78e0a0ad4b46b982371a59c456187df620
邓星玉 <1336797684@qq.com>
2020-06-03 17:30:31 +0800

    1

--- xcx_workman1/utils/util.js
+++ xcx_workman1/utils/util.js
@@ -9,6 +9,14 @@ const formatTime = date => {
   return [year, month, day].map(formatNumber).join('/') + ' ' + [hour, minute, second].map(formatNumber).join(':')
 }
 
+const formatDate = date => {
+  const year = date.getFullYear()
+  const month = date.getMonth() + 1
+  const day = date.getDate()
+  
+  return [year,month,day].map(formatNumber).join('-')
+}
+
 const formatNumber = n => {
   n = n.toString()
   return n[1] ? n : '0' + n
@@ -74,6 +82,7 @@ const FormatPhone = function (str) {
 
 module.exports = {
   formatTime: formatTime,
+  formatDate:formatDate,
   ages:Ages,
   formatName:FormatName,
   subName:SubName,
